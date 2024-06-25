a95d82b6beef8c5eca772d1dfaa4365570e75739
Timothy T. Joe <106974662+TimTJoe@users.noreply.github.com>
2024-06-25 13:26:33 +0000

    fixed theme switching bug

--- blog/frontend/src/App.jsx
+++ blog/frontend/src/App.jsx
@@ -1,12 +1,17 @@
 import { Button, Slider } from '@nextui-org/react';
 import "./App.css"
 import { ThemeSwitcher } from './components/themeSwitcher';
+import { ThemeProvider, useTheme } from 'next-themes'
 
 function App() {
+  const { theme, setTheme } = useTheme()
+
   return (
-    <main className="dark text-foreground bg-background">
-      <ThemeSwitcher />
-    </main>
+    <ThemeProvider themes={theme} >
+      <main className={`${theme} text-foreground bg-background`} >
+        <ThemeSwitcher />
+      </main>
+    </ThemeProvider>
   );
 }
 
--- blog/frontend/src/main.jsx
+++ blog/frontend/src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { NextUIProvider } from '@nextui-org/react'
-import { ThemeProvider } from 'next-themes'
 
 import App from './App.jsx'
 import './index.css'
@@ -9,9 +8,7 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <NextUIProvider>
-      <ThemeProvider>
-          <App />
-      </ThemeProvider>
+      <App />
     </NextUIProvider>
   </React.StrictMode>,
 )
