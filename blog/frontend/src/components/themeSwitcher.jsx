6fe4fcd21f3443fb53b3d0e3c3fcbcd078f55616
Timothy T. Joe <106974662+TimTJoe@users.noreply.github.com>
2024-06-25 13:40:40 +0000

    added button for themeswitcher

--- blog/frontend/src/components/themeSwitcher.jsx
+++ blog/frontend/src/components/themeSwitcher.jsx
@@ -1,15 +1,34 @@
 // components/ThemeSwitcher.tsx
 import { useTheme } from "next-themes";
 import { Button } from "@nextui-org/react";
+import { HiMoon, HiSun } from "react-icons/hi";
+import { useState } from "react";
 
 export const ThemeSwitcher = () => {
     const { theme, setTheme } = useTheme()
 
+    const handleSwitch = () => {
+        if (theme == "light") {
+            setTheme('dark')
+        } else if (theme == "dark") {
+            setTheme("light")
+        }
+    }
+
     return (
         <div>
+
             The current theme is: {theme}
-            <Button  color="primary" onClick={() => setTheme('light')}>Light Mode</Button>
-            <Button color="primary" onClick={() => setTheme('dark')}>Dark Mode</Button>
+            <Button
+                color="primary"
+                isIconOnly
+                onClick={handleSwitch}
+            >
+                {
+                    theme == "light" ? <HiSun /> : <HiMoon />
+                }
+            </Button>
+
         </div>
     )
 };
