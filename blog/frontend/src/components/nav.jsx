42d67c6b4913a4d9381e55cf521ccf7b6530993e
Timothy T. Joe <106974662+TimTJoe@users.noreply.github.com>
2024-07-03 15:29:46 +0000

    added buttons to navigation

--- blog/frontend/src/components/nav.jsx
+++ blog/frontend/src/components/nav.jsx
@@ -4,6 +4,8 @@ const Navbar = () => {
         <nav>
           <h5 class="max">CSA Blog</h5>
           <button class="circle transparent">
+            <a className="button border" href="/signin">Sign in</a>
+            <a className="button border" href="/signup">Sign Signup</a>
             <img class="responsive" src="/favicon.png"/>
           </button>
         </nav>
