84fa13350171a68071a1f918c777c8685723181e
Timothy T. Joe <106974662+TimTJoe@users.noreply.github.com>
2024-07-03 15:58:26 +0000

    change to browser router

--- blog/frontend/src/main.jsx
+++ blog/frontend/src/main.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { RouterProvider, BrowserRouter, Outlet } from "react-router-dom";
+import { RouterProvider, BrowserRouter, Outlet, Routes, Route } from "react-router-dom";
 import { Routers } from './router.jsx';
 
 import './index.css'
 import "beercss"
 import "material-dynamic-colors"
+import Home from '@pages/home/index.js';
+import SignupPage from '@pages/register/signup.page.jsx';
+import Signin from '@pages/signin/index.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={Routers} />
-    <Outlet />
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/signup' element={<SignupPage />} />
+      <Route path='/signin' element={<Signin />} />
+    </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
